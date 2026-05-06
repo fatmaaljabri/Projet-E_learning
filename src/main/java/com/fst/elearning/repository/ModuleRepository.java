a456ee20414e371d960ff0999df9948103a8283a
Eyaa <dridie087@gmail.com>
2026-05-07 00:33:59 +0100

    ss

--- src/main/java/com/fst/elearning/repository/ModuleRepository.java
+++ src/main/java/com/fst/elearning/repository/ModuleRepository.java
@@ -2,8 +2,13 @@ package com.fst.elearning.repository;
 
 import com.fst.elearning.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ModuleRepository extends JpaRepository<Module, Long> {
+    
     List<Module> findByCours_IdOrderByOrdreAsc(Long coursId);
+
+    @Query("SELECT m FROM Module m JOIN FETCH m.cours")  // ← ajoute ça
+    List<Module> findAllWithCours();
 }
